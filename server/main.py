a51c312ed039f39e5fe045a22f15af358f118a62
JoaoVargas <jotaguivargas@gmail.com>
2026-03-25 20:47:57 -0300

    add templates and update main.py for HTML response handling

--- server/main.py
+++ server/main.py
@@ -1,20 +1,30 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from models.example import fetch_all_from_table
 
 app = FastAPI()
+templates = Jinja2Templates(directory="./templates")
 
 
 @app.get("/")
 def get_root():
     return f"Hello!"
 
+
 @app.get("/{name}")
 def get_root_param(name: str | None = "World"):
     return f"Hello, {name}!"
 
-@app.get("/test/{name}")
-def get_test_param(name: str):
-    print(fetch_all_from_table(name))    
 
-    return {}
+@app.get("/test/{name}")
+def get_test_param(request: Request, name: str):
+    data = fetch_all_from_table(name)
+
+    print(f"Fetched data from table '{name}': {data}")
+
+    return templates.TemplateResponse(
+        request,
+        name="table.html",
+        context={"request": request, "name": name, "data": data},
+    )
