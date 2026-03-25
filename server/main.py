7835633761ded76028890506d9124e3eb850e3f1
JoaoVargas <jotaguivargas@gmail.com>
2026-03-25 19:53:22 -0300

    api changes

--- server/main.py
+++ server/main.py
@@ -5,8 +5,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return f"Hello!"
 
+@app.get("/{name}")
+def read_root_param(name: str | None = "World"):
+    return f"Hello, {name}!"
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
